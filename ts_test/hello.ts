6ae96c909e0727aa700f3cefb2257af4da447ba3
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-11-28 11:03:41 +0900

    update

--- ts_test/hello.ts
+++ ts_test/hello.ts
@@ -1,4 +1,19 @@
+// TypeInference で string型が自動的に設定
 var message = "Hello!!!!";
+// string型に number型は入れられない
+// message = 4;
 if (message) {
     console.log(message);
 }
+
+var id = 1;
+var email = "test@test.com";
+var isActive = true;
+var none;
+
+// id = "東京"
+// email = 5
+// isActive = "横浜"
+
+none = "YSE";
+none = 5;
