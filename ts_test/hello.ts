ec54391ffbcf8ab88c03b87298e68beda187b162
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-11-28 11:16:17 +0900

    update

--- ts_test/hello.ts
+++ ts_test/hello.ts
@@ -6,10 +6,17 @@ if (message) {
     console.log(message);
 }
 
+// Type Inference （型推論）
 var id = 1;
 var email = "test@test.com";
 var isActive = true;
 var none;
+
+// Type Definition （型定義）
+var id:number = 1;
+var email:string = "test@test.com";
+var isActive:boolean = true;
+var none:any;
 
 // id = "東京"
 // email = 5
