190d424291e2c16a9693bb0dd9e8c49578b410bf
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-05 09:42:23 +0900

    update

--- ts_test/item.ts
+++ ts_test/item.ts
@@ -1,3 +1,20 @@
 var itemNames:string[] = ["コーヒー", "紅茶", "ほうじ茶"];
 console.log(itemNames)
 
+var item1 = {
+    id: 1,
+    name: "コーヒー",
+    price: 350,
+}
+var item2 = {
+    id: 2,
+    name: "紅茶",
+    price: 400,
+}
+var item3 = {
+    id: 3,
+    name: "ほうじ茶",
+    price: 300,
+}
+var items:object[] = [item1, item2, item3];
+console.log(items)
