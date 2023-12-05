7a181812ba859614f59c9de38f8b5a8e0e60641e
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-05 10:19:58 +0900

    update

---
+++ ts_test/user.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = {
+    id: 1,
+    name: {
+        first: "Taro",
+        last: "Tokyo",
+    },
+    email: "test@test.com",
+    isActive: true,
+};
+console.log(user);
+// var user: {
+//     id: number;
+//     name: {
+//         first: string;
+//         last: string;
+//     };
+//     email: string;
+//     isActive: boolean;
+// } = {
+//     id: 1,
+//     name: {
+//         first: "Taro",
+//         last: "Tokyo",
+//     },
+//     email: "test@test.com",
+//     isActive: true,
+// }
