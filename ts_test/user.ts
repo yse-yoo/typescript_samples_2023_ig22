d66b1220053a068ee67289efc2813f9519edcd7a
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-05 09:26:30 +0900

    update

--- ts_test/user.ts
+++ ts_test/user.ts
@@ -1,9 +1,17 @@
 var user: {
     id: number;
+    name: {
+        first: string;
+        last: string;
+    };
     email: string;
     isActive: boolean;
 } = {
     id: 1,
+    name: {
+        first: "Taro",
+        last: "Tokyo",
+    },
     email: "test@test.com",
     isActive: true,
 }
@@ -12,7 +20,11 @@ console.log(user)
 
 //型推論（Type Inference）  
 // var user  = {
-//     id: 1,
+//      id: 1,
+//      name: {
+//         first: "Taro",
+//         last: "Tokyo",
+//     },
 //     email: "test@test.com",
 //     isActive: true,
 // }
